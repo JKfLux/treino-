77514cd3755a7ea5f0005b27b69f0795a3ced3f7
João Koch <147113779+JKfLux@users.noreply.github.com>
2023-12-06 16:34:23 -0300

    Parte1

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <section>
+    <header>Anna Carolina Pires Reinhardt Koch</header>
+    <main>
+     <h1>
+Sejam bem-vindos à minha página profissional!
+     </h1>
+    </main>
+    <footer></footer>
+  </section> 
+
+
+
+
+
+
+
+
+
+
+
+
+  )
+}
